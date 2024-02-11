ee6068f49e02432a96e88e63cbd7a2affb84b088
Cesar Melendez <cesarmelendez26@gmail.com>
2024-02-11 16:13:18 -0600

    Adición de la funcionalidad de desencriptar un texto

--- index.js
+++ index.js
@@ -15,13 +15,39 @@ function encriptar() {
     if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado con éxito";
-        parrafo.textContent ="";
-        muñeco.src ="./Imagenes/encriptado.jpg";
+        parrafo.textContent = "";
+        muñeco.src = "./Imagenes/encriptado.jpg";
     } else {
-        muñeco.src ="./Imagenes/muñeco.png";
+        muñeco.src = "./Imagenes/muñeco.png";
         tituloMensaje.textContent = "Ningun mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-        alert("Debes ingresar algún texto");
+        swal("Ooops!", "Debes ingresar algún texto", "warning");
     }
         
+}
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+        if (texto.length != 0) {
+            document.getElementById("texto").value = textoCifrado;
+            tituloMensaje.textContent = "Texto desencriptado con éxito";
+            parrafo.textContent ="";
+            muñeco.src = "./Imagenes/desencriptado.jpg";
+        } else {
+            muñeco.src ="./Imagenes/muñeco.png";
+            tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+            parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+            swal("Ooops!", "Debes ingresar algún texto", "warning");
+        }
 }
